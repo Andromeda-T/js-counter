21039f871d3ae71f647024f8c9c636bb123842cc
Radin T <radin.khodadadi0211@gmail.com>
2025-03-09 12:26:58 +0330

    add timer and suc and load msg

--- src/script.js
+++ src/script.js
@@ -1,15 +1,45 @@
-let inp_s = document.querySelector('#input-s');
-let btn = document.querySelector('#btn');
 
-btn.addEventListener('click' , function () {
-    let seconds = parseInt(inp_s.value)
-    let eror = document.querySelector('#eror')
-    if(isNaN(seconds)){
-        eror.classList.remove('hidden')
-    }
-    else{
-        eror.classList.add('hidden')
-    }
-    
-    // console.log(seconds)
+// selectors
+
+let box_s = document.querySelector('#box_s');
+let inp_s = document.querySelector('#inp_s');
+let btn_s = document.querySelector('#btn_s');
+let err_s = document.querySelector('#err_s');
+let cir_s = document.querySelector('#cir_s');
+let spn_s = document.querySelector('#spn_s');
+let goo_s = document.querySelector('#goo_s');
+let suc_s = document.querySelector('#suc_s');
+
+// items 
+
+btn_s.addEventListener('click', function () {
+    let secound = parseInt(inp_s.value);
+    let isValid = !isNaN(secound);
+    cir_s.classList.toggle('hidden', !isValid);
+    err_s.classList.toggle('hidden', isValid);
+    box_s.classList.toggle('hidden', isValid);
 })
+
+// timer
+
+btn_s.addEventListener('click', function () {
+    let secound = parseInt(inp_s.value);
+    spn_s.textContent = secound
+    goo_s.classList.remove('hidden')
+    suc_s.classList.add('hidden')
+    inp_s.value = ''
+
+    let timer = setInterval(() => {
+        if (secound <= 1) {
+            clearInterval(timer)
+            box_s.classList.remove('hidden');
+            suc_s.classList.remove('hidden')
+            goo_s.classList.add('hidden')
+            cir_s.classList.add('hidden')
+        }
+        secound--
+        spn_s.textContent = secound
+
+    }, 1000);
+})
+
